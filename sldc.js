84d627fdf2cd9ceaa4f93957607e018bc062b2f7
GeTtygreen <94148141+GeTtygreen@users.noreply.github.com>
2022-01-07 10:09:31 -0600

    onedown

--- sldc.js
+++ sldc.js
@@ -12,6 +12,13 @@
 // Planning
 // Testing & Integration
 
+planning
+Analysis
+Design
+Implementation
+testing& integration
+Maintain
+
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -19,3 +26,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const Analysis = "this is the second step, in which allows the engineers of the software to define the user needs early in the development process"
+const Design ='This is the third step, in which the design is developed accordind to the specifications written during the two previous stages '
+const Implementation ='This is the fourth step , this is the coding stage each programmer has his/her own software development task list in which he/she is resposible for '
+const TestingIntegration = 'This is the fourth step , here we would test /trial and error / the code to figure out how many issues,conflicts,and bugs are there '
+Maintain = 'This is the sixth step , in this stage once the software is put out to the market developers must not keep up maintenance handling issues reported by end-users'
