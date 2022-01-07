3ccd6e6d292ef95ee3d47e4db8732619b0f0cb3d
GeTtygreen <94148141+GeTtygreen@users.noreply.github.com>
2022-01-07 10:33:33 -0600

    2down

--- variables.js
+++ variables.js
@@ -2,24 +2,30 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
+let myName = "Nicholas Getty Green "
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
+let faveNum = 5; 
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
-
+let lovesCode = true ; 
 //////////////////PROBLEM 4////////////////////
 /*
   Write an if-statement that checks if the faveNum variable is 
   equal to 13. If it is, console.log('lucky 13!'). 
   If it's not, console.log('not lucky 13').
 */
-
+if(faveNum == 13){
+  console.log('lucky 13')
+}else{
+  console.log('not lucky 13') 
+}
 //CODE HERE
 
 //////////////////PROBLEM 5////////////////////
@@ -28,3 +34,6 @@
 */
 
 //CODE HERE
+for (let gt = 5 ; gt < 6; gt===5 ){
+  console.log(gt)
+}
